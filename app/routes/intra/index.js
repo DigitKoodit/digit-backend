2543122663ae5ca930126a96287559b8311abbd3
niemisami <sami@nieminen.fi>
2018-07-25 17:36:12 +0300

    Simplify intra routes by removing cms part

--- app/routes/intra/index.js
+++ app/routes/intra/index.js
@@ -3,8 +3,8 @@ const { decoratePublic } = require('../../models/userAccount/userAccountDecorato
 
 // request is populated with user information during jwt authentication. See routes/index.js
 router.get('/profile', (req, res) => Promise.resolve(decoratePublic(req.user)).then(user => res.send(user)))
-router.use('/accounts', require('./userManagement'))
-router.use('/cms/content', require('./siteContent').router)
-router.use('/cms/sponsor', require('./sponsor').router)
+router.use('/account', require('./userManagement'))
+router.use('/content', require('./siteContent').router)
+router.use('/sponsor', require('./sponsor').router)
 
 module.exports = router
