5ec08afa9ef14163e9b2022ff906508fe52bcf19
niemisami <sami@nieminen.fi>
2018-08-29 18:16:57 +0300

    Add FACEBOOK_ACCESS_TOKEN empty env var

--- digit_dev.config.js
+++ digit_dev.config.js
@@ -11,7 +11,8 @@ module.exports = {
       },
       env: {
         NODE_ENV: 'development',
-        SECRET_KEY: 'dc94b4de1cb5e4e80be006c59752b43aaa3fc19837f2d1470a751dc28ffeed36'
+        SECRET_KEY: 'dc94b4de1cb5e4e80be006c59752b43aaa3fc19837f2d1470a751dc28ffeed36',
+        FACEBOOK_ACCESS_TOKEN: ''
       },
       node_args: '--inspect=0.0.0.0:9230'
     }
