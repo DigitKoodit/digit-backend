f796b8d1da63dced66a61b664eb755a1500e04dc
niemisami <sami@nieminen.fi>
2018-10-10 23:10:47 +0300

    Handle SIGTERM and SIGINT signals

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,5 @@
 
 const app = require('../app')
-
 const normalizePort = val => {
   const port = parseInt(val, 10)
   if(isNaN(port)) {
@@ -16,6 +15,7 @@ const port = normalizePort(process.env.PORT || '3001')
 app.set('port', port)
 
 const migrate = require('../db/migrate')
+const { db, pgp } = require('../db/pgp')
 
 migrate.up()
   .then(() => {
@@ -25,3 +25,31 @@ migrate.up()
     console.error(err)
     process.exit(1)
   })
+
+const gracefulShutdown = () => {
+  console.log('Received kill signal, shutting down gracefully.')
+  app.close(() => {
+    console.log('object', pgp)
+    db.proc('version')
+      .then(data => {
+        console.log('CONNECTION ACYIVE')
+        // SUCCESS
+        // data.version =
+        // 'PostgreSQL 9.5.1, compiled by Visual C++ build 1800, 64-bit'
+      })
+      .catch(error => {
+        console.log('EREOROR', error)
+      })
+    console.log('Closed out remaining connections.')
+    process.exit(0)
+  })
+  setTimeout(() => {
+    console.error('Could not close connections in time, forcefully shutting down')
+    process.exit(0)
+  }, 10 * 1000)
+}
+
+// TERM signal e.g. kill
+process.on('SIGTERM', gracefulShutdown)
+// TERM signal e.g. ctrl + c
+process.on('SIGINT', gracefulShutdown)
