ff2e0507f61bfc9eb6980e32f75765fac560fb80
niemisami <sami@nieminen.fi>
2019-02-25 20:29:14 +0200

    Sort enrolls by createdAt date
    TODO: allow admin define the sort order

--- app/models/event/eventEnrollDecorators.js
+++ app/models/event/eventEnrollDecorators.js
@@ -1,35 +1,45 @@
 const filter = require('lodash/filter')
-const reduce = require('lodash/reduce')
+const moment = require('moment')
 
 const decoratePublic = eventEnroll => {
   const { event_enroll_id: id, event_id: eventId, event_enroll_data, fields } = eventEnroll
-  const { isSpare, values = {} } = event_enroll_data
+  const { isSpare, values = {}, createdAt } = event_enroll_data
   const publicFields = filter(fields, 'public')
   const publicValues = publicFields.reduce((acc, field) => ({ ...acc, [field.name]: values[field.name] }), {})
   return {
     id,
     eventId,
     values: publicValues,
-    isSpare
+    isSpare,
+    createdAt: moment(createdAt).format()
   }
 }
 
 const decorate = eventEnroll => {
   const { event_enroll_id: id, event_id: eventId, event_enroll_data } = eventEnroll
-  const { isSpare = false, values } = event_enroll_data
+  const { isSpare = false, values, createdAt } = event_enroll_data
   return {
     id,
     eventId,
     values,
-    isSpare
+    isSpare,
+    createdAt: moment(createdAt).format()
   }
 }
 
 const decorateList = events =>
-  events.map(decorate)
+  events.map(decorate).sort((a, b) =>
+    moment(a.createdAt).isBefore(b.createdAt)
+      ? -1
+      : 1
+  )
 
 const decoratePublicList = events =>
-  events.map(decoratePublic)
+  events.map(decoratePublic).sort((a, b) =>
+    moment(a.createdAt).isBefore(b.createdAt)
+      ? -1
+      : 1
+  )
 
 module.exports = {
   decorate,
