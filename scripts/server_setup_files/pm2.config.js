939ad64b21a41c9b48e5d6062c4ec6a63d659d8a
niemisami <sami@nieminen.fi>
2020-12-20 17:27:06 +0200

    Add database RDS instance route

--- scripts/server_setup_files/pm2.config.js
+++ scripts/server_setup_files/pm2.config.js
@@ -7,11 +7,12 @@ module.exports = {
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
+        PORT: 3000,
         PGDATABASE: 'digit',
         PGUSER: 'digit',
-        PGPASSWORD: 'A3M.}bzL7Ugj;t@',
-        PGHOST: 'localhost',
-        PORT: '3002',
+        PGHOST: 'elmeri-database.cinnv1kp1too.eu-north-1.rds.amazonaws.com',
+        PGPASSWORD: '*Y64o&htrJ6!$gPXHR',
+        PGPORT: '5432',
         SECRET_KEY: '+DgUn4RB:Rs}J%;7)tY(K@jgE6r/K6',
         FACEBOOK_ACCESS_TOKEN: '225172908136009|kPY5ms0wFLs5W5B1EuRdyHjsgWs'
       }
